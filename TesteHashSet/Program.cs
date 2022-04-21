347dcec9e2e08f4b2964c7748e8ca506749c17f5
Rodrigo Sandeski <sandeski47@gmail.com>
2022-04-21 19:24:26 -0300

    Finish Example

--- TesteHashSet/Program.cs
+++ TesteHashSet/Program.cs
@@ -9,16 +9,36 @@ namespace TesteHashSet
         static void Main(string[] args)
         {
             SortedSet<int> a = new SortedSet<int>() {0,2,4,5,6,8,10 };
-            SortedSet<int> b = new SortedSet<int>() { 5,6,7,8,9,10 };
+            SortedSet<int> b = new SortedSet<int>() { 5,6,7,8,9,10,11 };
 
-            
+
+            //union
+            SortedSet<int> c = new SortedSet<int>(a);
+            c.UnionWith(b);
+            PrintCollection(c);
+
+
+            //intersection elementos que estão nos dois conjuntos
+            SortedSet<int> d = new SortedSet<int>(a);
+            d.IntersectWith(b);
+            PrintCollection(d);
+
+            //Difference
+            SortedSet<int> e = new SortedSet<int>(a);
+            e.ExceptWith(b);
+            PrintCollection(e);
+
 
             Console.ReadKey();
         }
 
-        static void PrintColection<T>(IEnumerable<T> collection)
+        static void PrintCollection<T>(IEnumerable<T> collection)
         {
-
+            foreach(T obj in collection)
+            {
+                Console.Write(obj + " ");
+            }
+            Console.WriteLine();
         }
     }
 }
