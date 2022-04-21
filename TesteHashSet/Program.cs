73be64f07bd05c94123788595498874c6d0c6bc6
Rodrigo Sandeski <sandeski47@gmail.com>
2022-04-21 18:28:38 -0300

    first commit

---
+++ TesteHashSet/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TesteHashSet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedSet<int> a = new SortedSet<int>() {0,2,4,5,6,8,10 };
+            SortedSet<int> b = new SortedSet<int>() { 5,6,7,8,9,10 };
+
+            
+
+            Console.ReadKey();
+        }
+
+        static void PrintColection<T>(IEnumerable<T> collection)
+        {
+
+        }
+    }
+}
